4beff49f0d838d3823fa5f5998577a2d9c91aa8a
Ardian Sallauka <ardi_c_cc@hotmail.com>
2026-05-06 17:52:19 +0200

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-CrIZir-0.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/ardiansallauka/Projects/earthquake-explorer/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/earthquake-explorer/assets/index-Jbj_mvlm.js"] }, "/": { filePath: "/Users/ardiansallauka/Projects/earthquake-explorer/src/routes/index.tsx", children: void 0, assets: [{ tag: "link", attrs: { rel: "stylesheet", href: "/earthquake-explorer/assets/index-CIGW-MKW.css", type: "text/css" } }], preloads: ["/earthquake-explorer/assets/index-Qqe24O2Z.js"] } }, clientEntry: "/earthquake-explorer/assets/index-Jbj_mvlm.js" });
+export {
+  tsrStartManifest
+};
